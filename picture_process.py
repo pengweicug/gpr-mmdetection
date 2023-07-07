afe42535fbe4a89e32d7be0fc02fbf644a0d863d
pengweicug <37527543+pengweicug@users.noreply.github.com>
2023-07-07 16:54:33 +0800

    Update picture_process.py

--- picture_process.py
+++ picture_process.py
@@ -14,21 +14,24 @@ class LongPictureProcess:
     #读取图片
     img=Image.open(img_path)
     #img = mmcv.imread(img_path, channel_order='rgb')
-    #判定图片是否需要切割，按照2：1规格进行切割，直到不足2000像素为止，再以切割处为中心进行二次切割
+    #判定图片是否需要切割，按照2：1规格进行切割，直到不足2h像素为止，再以切割处为中心进行二次切割
     w,h=img.size
-    #第一次切片
     i=1
     while w-h*2*i>0:
+      #第一次切片
       n=((i-1)*2*h,0,i*2*h,h)
       temp = img.crop(n)
-      temp.save(path.replace(".jpg", str(i - 1) + '.jpg'))
-      i=i+1
-    #第二次切片
-    j=1
-    while w-h*2*i>0:
-      n=((i-1)*2*h,0,i*2*h,h)
-      temp = img.crop(n)
-      temp.save(path.replace(".jpg", str(i - 1) + '.jpg'))
+      #第一次切片图片名为单数
+      temp.save(img_path.replace(".jpg", str(2*i-1) + '.jpg'))
+      #第二次切片
+      if w-h*3*i>=0：
+        m=((i-1)*2*h+h,0,i*2*h+h,h)
+        temp = img.crop(m)
+      else:
+        m=((i-1)*2*h+h,0,w,h)
+        temp = img.crop(m) 
+      #第二次切片图片名为双数
+      temp.save(img_path.replace(".jpg", str(2*i) + '.jpg'))
       i=i+1
   #图片缝合函数
   def joint(self,img_path:str):
