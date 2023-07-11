d64f3833e73c421e3d5808d8c5d8608237d838da
pengwei <pengwei24@163.com>
2023-07-11 12:09:44 +0800

    11

--- picture_process.py
+++ picture_process.py
@@ -1,16 +1,14 @@
 # Copyright (c) pengwei. All rights reserved.
 
-from mmdet.apis import init_detector, inference_detector
-from mmdet.utils import register_all_modules
-from mmdet.registry import VISUALIZERS
-import mmcv
 from PIL import Image
+from mmdet.apis import init_detector, inference_detector
+from mmdet.registry import VISUALIZERS
 
-#超长图片切割缝合类
 
-class LongPictureProcess:
-  #图片分片函数
-  def slicing(self,img_path:str):
+#超长图片切割缝合
+#图片分片函数
+def slicing(self,img_path:str,config_file,checkpoint_file):
+    model = init_detector(config_file, checkpoint_file, device='cuda:0')
     #读取图片
     img=Image.open(img_path)
     #img = mmcv.imread(img_path, channel_order='rgb')
@@ -22,21 +20,64 @@ class LongPictureProcess:
       n=((i-1)*2*h,0,i*2*h,h)
       temp = img.crop(n)
       #第一次切片图片名为单数
-      temp.save(img_path.replace(".jpg", str(2*i-1) + '.jpg'))
+      #temp.save(img_path.replace(".jpg", str(2*i-1) + '.jpg'))
+      first_result = first_result+self.adjust_coordinate(temp,i,h,model)
       #第二次切片
-      if w-h*3*i>=0：
+      if w-h*3*i>=0:
         m=((i-1)*2*h+h,0,i*2*h+h,h)
-        temp = img.crop(m)
       else:
         m=((i-1)*2*h+h,0,w,h)
-        temp = img.crop(m) 
+      temp = img.crop(m)
       #第二次切片图片名为双数
-      temp.save(img_path.replace(".jpg", str(2*i) + '.jpg'))
+      #temp.save(img_path.replace(".jpg", str(2*i) + '.jpg'))
+      second_result = second_result + self.adjust_coordinate(temp, i, h,model)
       i=i+1
-  #图片缝合函数
-  def joint(self,img_path:str):
-    model = init_detector(config_file, checkpoint_file, device='cuda:0')
-    result = inference_detector(model, img)
+    result=merge_target(first_result, second_result)
+    pic_visualizer(result, img, model)
+
+def adjust_coordinate(temp,i,h,model):
+    result = inference_detector(model, temp)
+    pred_instances = result.pred_instances
+    bboxes = pred_instances.bboxes
+    for box in bboxes:
+      box[0] = box[0] + (i - 1) * 2 * h
+      box[2] = box[2] + (i - 1) * 2 * h
+    return result
+
+def merge_target(first_result,second_result):
+  first_pred_instances = first_result.pred_instances
+  first_bboxes = first_pred_instances.bboxes
+  second_pred_instances = second_result.pred_instances
+  second_bboxes = second_pred_instances.bboxes
+  for first_box in first_bboxes:
+    for second_box in second_bboxes:
+      if((second_box[0]<first_box[0]<second_box[2] and second_box[1]<first_box[1]<second_box[3])
+              or (second_box[0]<first_box[2]<second_box[2] and second_box[1]<first_box[3]<second_box[3])):
+        first_bboxes.remove(first_box)
+
+  return result
+
+
+def pic_visualizer(result,img,model):
+    visualizer = VISUALIZERS.build(model.cfg.visualizer)
+    visualizer.dataset_meta = model.dataset_meta
+    visualizer.add_datasample(
+      'result',
+      img,
+      data_sample=result,
+      draw_gt=False,
+      wait_time=0,
+    )
+    visualizer.show()
+
+if __name__ == '__main__':
+  slicing()
+
+
+
+
+
+
 
 
 
